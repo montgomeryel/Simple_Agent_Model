762e7074f46ef4925a19685193846ef56706b022
montgomeryel <164370729+montgomeryel@users.noreply.github.com>
2024-05-16 10:59:45 -0500

    update to py: happiness if statements

--- Agent_Model_attempt.py
+++ Agent_Model_attempt.py
@@ -7,15 +7,38 @@ Created on Thu May 16 09:56:30 2024
 
 #We want an overarching world class that has an init function that 
 ##builds a grid, builds agents, and detects vacant positions
-##the agent class should determing if happy and move if not
+##the agent class should determine if happy and move if not
+#world needs to have a run method
 #visualization: https://github.com/prasenjit52282/GridWorld
     #http://modelingcommons.org/account/login
+
+#setting up the world
+parameters = {'world_size':(10,10),
+          'num_agents':40,
+          'same_pref' :0.4, #desires 40% of neighbors to be same color
+          'max_iter'  :100,
+          'out_path'  :r'C:\Users\Ella\Documents\GitHub\Simple_Agent_Model\abm_results.csv' }
+
 class Agent():
-    def __init__():
-        #needs to know color and prefrence
+    def __init__(self, color, pref):
+        #needs to know prefrence and location
+        self.color = color
+        self.pref = pref
+        self.location = None
         pass
     def move(self):
         #decide if it wants to move
         #move to new position if it does
-        #
-        pass
+        #need to check if happy first 
+      happy = self.am_i_happy
+      
+      if not happy:
+          vacancies = self.world.find_vacant(return_all=True) # returns all empty grid spots
+          for patch in vacancies:
+              i_moved = False
+              likeit = self.am_i_happy
+              if likeit is True:
+                  self.world.grid[self.location] = None
+                  self.location = patch
+                  self.world.grid[patch] = self
+                  i_moved = True
