fdbb446a0e5f7dfb0d6b2933319875c382217dd0
montgomeryel <164370729+montgomeryel@users.noreply.github.com>
2024-05-16 10:21:10 -0500

    Writing pseudocode

---
+++ Agent_Model_attempt.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 09:56:30 2024
+
+@author: Ella
+"""
+
+#We want an overarching world class that has an init function that 
+##builds a grid, builds agents, and detects vacant positions
+##the agent class should determing if happy and move if not
+#visualization: https://github.com/prasenjit52282/GridWorld
+    #http://modelingcommons.org/account/login
+class Agent():
+    def __init__():
+        #needs to know color and prefrence
+        pass
+    def move(self):
+        #decide if it wants to move
+        #move to new position if it does
+        #
+        pass
